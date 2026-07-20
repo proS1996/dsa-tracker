91f580105a90dbedacd077181671424961bafbbc
proS1996 <proS1996@users.noreply.github.com>
2026-07-20 05:34:50 +0000

    Add LinkedIn share button for progress sharing, OG meta tags, bump SW cache to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dsa-tracker-v7";
+const CACHE = "dsa-tracker-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png",
   "./apple-touch-icon.png", "./apple-touch-icon-152.png", "./apple-touch-icon-167.png"];
 
